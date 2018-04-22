b598ec490af8407342faa08e79eef9af0ae34d5d
Keith Lashley <lashleykeith@gmail.com>
2018-04-22 22:00:47 +0900

    Add files via upload

--- our_tutorial/5finalrenamingapp/app3.go
+++ our_tutorial/5finalrenamingapp/app3.go
@@ -1,11 +1,10 @@
-
 package main
  
 import (
     "log"
     "os"
     "fmt"
-    "strings"
+    "regexp"
 )
 func readCurrentDir() {
     file, err := os.Open("C:\\Users\\g\\Desktop\\prank")
@@ -17,12 +16,15 @@ func readCurrentDir() {
     list,_ := file.Readdirnames(0) // 0 to read all files and folders
     for _, name := range list {
         fmt.Println("Old Name - ", name)
-        fmt.Println("New Name - ", strings.Replace(name, "0123456789", " ", -1))
+        re := regexp.MustCompile( "[^A-za-z]" )
+        newName := re.ReplaceAllString( name, " ")
+        fmt.Println("New Name - ", newName)
     }
 }
  
 func main() {
     readCurrentDir()
 }
+
 
 //http://www.golangprograms.com/how-to-read-names-of-all-files-and-folders-in-current-directory.html
